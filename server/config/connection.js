202dcab175bc45afb70972fe0a840073a347fae3
dsatpm <jeremiahjh@gmail.com>
2023-12-24 20:36:14 -0800

    push another debug

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+MONGODB_URI =
+	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/reduxStore_db';
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = mongoose.connection;
